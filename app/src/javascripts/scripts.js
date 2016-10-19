b14fc72314f5781e7e5b42917bed4e78caa6ecf8
Jarvis Johnson <jarviscjohnson@gmail.com>
2016-10-18 20:47:52 -0400

    after v2 changes

--- app/src/javascripts/scripts.js
+++ app/src/javascripts/scripts.js
@@ -5,6 +5,16 @@
  */
 
 $('.renders').slick({
+  centerMode: true,
+    infinite: true,
+  arrows: false,
+  variableWidth: true,
+  asNavFor: '.renders-text'
+});
+$('.renders-text').slick({
+  asNavFor: '.renders',
+    infinite: true,
+  arrows: true
 });
 
 $("#contact form input").after("<span class='after'>|</span>").before("<span class='before'>|</span>");
@@ -33,28 +43,17 @@ function getDocHeight() {
         D.body.clientHeight, D.documentElement.clientHeight
     );
 }
+
 var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ? true : false;
 
- 
-      
-
-
-
-$(document).ready(function(){
+function scrollingPattern() {
 
     var windowHeight = $(window).height();
-  
-  // Even when the window is resized, run this code.
-  // $(window).resize(function(){
-    
-    // Variables
-
     var fixedHeight = $('.fixed-wrap').height();
     if(isMobile) {
     $('.fixed-wrap').css('margin-top' , windowHeight);
     }else{};
 
-//    console.log(windowHeight);
 
     $('.intro, #about').height(windowHeight);
     //$( '.intro' ).css('margin-bottom' , fixedHeight);
@@ -63,13 +62,17 @@ $(document).ready(function(){
     // Find the value of 90% of the viewport height
     var ninetypercent = .9 * windowHeight;
     var eightypercent = .80 * windowHeight;
-    var fortypercent = .40 * windowHeight;
+    var fortypercent = .30 * windowHeight;
+    if(windowHeight < 400 ){
+      var fortypercent = windowHeight;
+    }
 
     if(isMobile) {
     $('#floor-plans .renders .render, .image-section').height(fortypercent);      
     }else{
     $('#floor-plans .renders .render, .image-section').height(eightypercent);      
     }
+
     // When the document is scrolled ninety percent, toggle the classes
     // Does not work in iOS 7 or below
     // Hasn't been tested in iOS 8
@@ -103,275 +106,20 @@ $(document).ready(function(){
       } 
 
     });
-
-  // Call it on resize.
-  // }).resize();
-  
-}); // jQuery
-
-
-// Google Maps Scripts
-var map = null;
-// When the window has finished loading create our google map below
-google.maps.event.addDomListener(window, 'load', init);
-google.maps.event.addDomListener(window, 'resize', function() {
-    map.setCenter(new google.maps.LatLng(-33.900327, 151.269124));
-});
-
-function init() {
-    // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-    var mapOptions = {
-        // How zoomed in you want the map to start at (always required)
-        zoom: 17,
-
-        // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(-33.900327, 151.269124), // 21 Thompson St Tamarama
-
-        // Disables the default Google Maps UI components
-        disableDefaultUI: true,
-        scrollwheel: false,
-        draggable: false,
-
-        // How you would like to style the map. 
-        // This is where you would paste any style found on Snazzy Maps.
-        styles:[
-  {
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#f5f5f5"
-      }
-    ]
-  },
-  {
-    "elementType": "labels.icon",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#616161"
-      }
-    ]
-  },
-  {
-    "elementType": "labels.text.stroke",
-    "stylers": [
-      {
-        "color": "#f5f5f5"
-      }
-    ]
-  },
-  {
-    "featureType": "administrative.land_parcel",
-    "elementType": "labels",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "administrative.land_parcel",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#bdbdbd"
-      }
-    ]
-  },
-  {
-    "featureType": "landscape",
-    "stylers": [
-      {
-        "color": "#cec9be"
-      }
-    ]
-  },
-  {
-    "featureType": "poi",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#eeeeee"
-      }
-    ]
-  },
-  {
-    "featureType": "poi",
-    "elementType": "labels.text",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "poi",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#757575"
-      }
-    ]
-  },
-  {
-    "featureType": "poi.business",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "poi.park",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#e5e5e5"
-      }
-    ]
-  },
-  {
-    "featureType": "poi.park",
-    "elementType": "labels.text",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "poi.park",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#9e9e9e"
-      }
-    ]
-  },
-  {
-    "featureType": "road",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#ffffff"
-      }
-    ]
-  },
-  {
-    "featureType": "road.arterial",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#757575"
-      }
-    ]
-  },
-  {
-    "featureType": "road.highway",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#dadada"
-      }
-    ]
-  },
-  {
-    "featureType": "road.highway",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#616161"
-      }
-    ]
-  },
-  {
-    "featureType": "road.local",
-    "elementType": "labels",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "road.local",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#9e9e9e"
-      }
-    ]
-  },
-  {
-    "featureType": "transit.line",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#e5e5e5"
-      }
-    ]
-  },
-  {
-    "featureType": "transit.station",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#eeeeee"
-      }
-    ]
-  },
-  {
-    "featureType": "water",
-    "stylers": [
-      {
-        "color": "#b4e3ff"
-      }
-    ]
-  },
-  {
-    "featureType": "water",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#9e9e9e"
-      }
-    ]
-  }
-]
-    };
-
-    // Get the HTML DOM element that will contain your map 
-    // We are using a div with id="map" seen below in the <body>
-    var mapElement = document.getElementById('map');
-
-    // Create the Google Map using out element and options defined above
-    map = new google.maps.Map(mapElement, mapOptions);
-
-    var pinColor = "0083cb";
-    var pinImage = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + pinColor,
-        new google.maps.Size(21, 34),
-        new google.maps.Point(0,0),
-        new google.maps.Point(10, 34));
-    var pinShadow = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_shadow",
-        new google.maps.Size(40, 37),
-        new google.maps.Point(0, 0),
-        new google.maps.Point(12, 35));    
-
-    // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
-    var image = '../images/map-marker.png';
-    var myLatLng = new google.maps.LatLng(-33.900327, 151.269124);
-    var beachMarker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        icon: pinImage,
-        shadow: pinShadow
-    });
 }
+
+// Even when the window is resized, run this code.
+$( document ).ready(function() {
+  scrollingPattern();
+});
+window.addEventListener("orientationchange", function() {
+  // Announce the new orientation number
+  scrollingPattern();
+}, false);
+if(!isMobile) {
+  $(window).resize(function(){
+      scrollingPattern();
+  });
+}
+  
+
