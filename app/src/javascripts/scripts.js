4bd3be1142e2bec092911055daa2eea1367367b4
Jarvis Johnson <jarviscjohnson@gmail.com>
2016-10-20 12:40:58 -0400

    more style + js changes

--- app/src/javascripts/scripts.js
+++ app/src/javascripts/scripts.js
@@ -1,39 +1,44 @@
-/*!
- * Start Bootstrap - Grayscale Bootstrap Theme (http://startbootstrap.com)
- * Code licensed under the Apache License v2.0.
- * For details, see http://www.apache.org/licenses/LICENSE-2.0.
- */
-
-$('.renders').slick({
-  centerMode: true,
-    infinite: true,
-  arrows: false,
-  variableWidth: true,
-  asNavFor: '.renders-text'
+var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ? true : false;
+var $slider = $('.renders')
+        .on('init', function(slick) {
+            console.log('fired!');
+            $('.renders').fadeIn(3000);
+        })
+        .slick({
+          centerMode: true,
+          infinite: true,
+          arrows: false,
+          variableWidth: true,
+          asNavFor: '.renders-text'
 });
+$('.render').addClass('initialized');
 $('.renders-text').slick({
-  asNavFor: '.renders',
+    asNavFor: '.renders',
     infinite: true,
-  arrows: true
+    arrows: true
 });
 
-$("#contact form input").after("<span class='after'>|</span>").before("<span class='before'>|</span>");
+if (!isMobile) {
 
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
+    $("#contact form input").after("<span class='after'>|</span>").before("<span class='before'>|</span>");
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $(function() {
+        $('a.page-scroll').bind('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1500, 'easeInOutExpo');
+            event.preventDefault();
+        });
     });
-});
 
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $(this).closest('.collapse').collapse('toggle');
-});
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $(this).closest('.collapse').collapse('toggle');
+    });
+
+}
 
 function getDocHeight() {
     var D = document;
@@ -44,217 +49,122 @@ function getDocHeight() {
     );
 }
 
-var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ? true : false;
-
 function scrollingPattern() {
 
     var windowHeight = ($(window).height() + 1);
     var fixedHeight = $('.fixed-wrap').height();
-    if(isMobile) {
-    $('.fixed-wrap').css('margin-top' , windowHeight);
-    }else{};
+    if (isMobile) {
+        $('.fixed-wrap').css('margin-top', windowHeight);
+    } else {};
 
 
     $('.intro, #about').height(windowHeight);
     //$( '.intro' ).css('margin-bottom' , fixedHeight);
-    $('body').css('height' , getDocHeight());
+    $('body').css('height', getDocHeight());
 
     // Find the value of 90% of the viewport height
-    var ninetypercent = .9 * windowHeight;
-    var eightypercent = .70 * windowHeight;
-    var fortypercent = .30 * windowHeight;
-    if(windowHeight < 400 ){
-      var fortypercent = windowHeight;
+    var ninetypercent = .90 * windowHeight;
+    var eightypercent = .80 * windowHeight;
+    var seventypercent = .68 * windowHeight;
+    var fortypercent = .40 * windowHeight;
+    if (windowHeight < 400) {
+        var fortypercent = windowHeight;
     }
 
-    if(isMobile) {
-    $('#floor-plans .renders .render, .image-section').height(fortypercent);      
-    }else{
-    $('#floor-plans .renders .render').height(eightypercent);      
-    $('.image-section').height(windowHeight);
+    if (isMobile) {
+        $('#floor-plans .renders .render img, .image-section').height(fortypercent);
+    } else {
+        $('#floor-plans .renders .render img').height(eightypercent);
+        $('.image-section').height(seventypercent);
     }
 
     // When the document is scrolled ninety percent, toggle the classes
     // Does not work in iOS 7 or below
     // Hasn't been tested in iOS 8
-    $(document).scroll(function(){
+    $(document).scroll(function() {
 
-      
-      // Store the document scroll function in a variable
-      var y = $(this).scrollTop();   
-      
-      // If the document is scrolled 90%
-      if( y > ninetypercent) {
-        
-        // Add the "sticky" class
-        $('nav').addClass('sticky');
-      } else {
-        // Else remove it.
-        $('nav').removeClass('sticky');
-      };
-     if(!isMobile) {
-        // If the document is scrolled 100%
-        if( y < windowHeight ) {
-          
-          // Add the "sticky" class
-          $('.fixed-wrap').addClass('fix');
+
+        // Store the document scroll function in a variable
+        var y = $(this).scrollTop();
+
+        // If the document is scrolled 90%
+        if (y > ninetypercent) {
+
+            // Add the "sticky" class
+            $('nav').addClass('sticky');
         } else {
-          // Else remove it.
-          $('.fixed-wrap').removeClass('fix').css('margin-top' , windowHeight);
-          $('body').css('height' , getDocHeight());
-
+            // Else remove it.
+            $('nav').removeClass('sticky');
         };
-      } 
+        if (!isMobile) {
+            // If the document is scrolled 100%
+            if (y < windowHeight) {
+
+                // Add the "sticky" class
+                $('.fixed-wrap').addClass('fix');
+            } else {
+                // Else remove it.
+                $('.fixed-wrap').removeClass('fix').css('margin-top', windowHeight);
+                $('body').css('height', getDocHeight());
+
+            };
+        }
 
     });
 }
 
-
-
-
 // Even when the window is resized, run this code.
-$( document ).ready(function() {
-  scrollingPattern();
-  console.log(isMobile);
+$(document).ready(function() {
+    scrollingPattern();
 });
 window.addEventListener("orientationchange", function() {
-  // Announce the new orientation number
-  scrollingPattern();
+    scrollingPattern();
 });
-if(!isMobile) {
-  $(window).resize(function(){
-      scrollingPattern();
-  });
+if (!isMobile) {
+    $(window).resize(function() {
+        scrollingPattern();
+    });
 }
-  
+
 (function($) {
 
-  /**
-   * Copyright 2012, Digital Fusion
-   * Licensed under the MIT license.
-   * http://teamdf.com/jquery-plugins/license/
-   *
-   * @author Sam Sehnert
-   * @desc A small plugin that checks whether elements are within
-   *     the user visible viewport of a web browser.
-   *     only accounts for vertical position, not horizontal.
-   */
+    /**
+     * Copyright 2012, Digital Fusion
+     * Licensed under the MIT license.
+     * http://teamdf.com/jquery-plugins/license/
+     *
+     * @author Sam Sehnert
+     * @desc A small plugin that checks whether elements are within
+     *     the user visible viewport of a web browser.
+     *     only accounts for vertical position, not horizontal.
+     */
 
-  $.fn.visible = function(partial) {
-    
-      var $t            = $(this),
-          $w            = $(window),
-          viewTop       = $w.scrollTop(),
-          viewBottom    = viewTop + $w.height(),
-          _top          = $t.offset().top,
-          _bottom       = _top + $t.height(),
-          compareTop    = partial === true ? _bottom : _top,
-          compareBottom = partial === true ? _top : _bottom;
-    
-    return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+    $.fn.visible = function(partial) {
 
-  };
-    
+        var $t = $(this),
+            $w = $(window),
+            viewTop = $w.scrollTop(),
+            viewBottom = viewTop + $w.height(),
+            _top = $t.offset().top,
+            _bottom = _top + $t.height(),
+            compareTop = partial === true ? _bottom : _top,
+            compareBottom = partial === true ? _top : _bottom;
+
+        return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+
+    };
+
 })(jQuery);
 
 $(window).scroll(function(event) {
-  
-  $(".below-renders").each(function(i, el) {
-    var el = $(el);
-    if (el.visible(true)) {
-      $('.navbar-fixed-bottom').addClass("hide"); 
-    } else {
-      $('.navbar-fixed-bottom').removeClass("hide");
-    }
-  });
-  
+
+    $(".below-renders").each(function(i, el) {
+        var el = $(el);
+        if (el.visible(true)) {
+            $('.navbar-fixed-bottom').addClass("hide");
+        } else {
+            $('.navbar-fixed-bottom').removeClass("hide");
+        }
+    });
+
 });
-
-
-function isScrolledIntoView(elem)
-{
-    var docViewTop = $(window).scrollTop();
-    var docViewBottom = docViewTop + $(window).height();
-
-    var elemTop = $(elem).offset().top -75;
-    var elemBottom = elemTop + $(elem).height() +175;
-
-    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-}
-
-
-function stopScroll(element) {
-
-   $(element).mousewheel(function(event, delta) {
-
-      this.scrollLeft -= (delta * 30);
-    
-      event.preventDefault();
-
-   });
-
-}
-
-$(document).scroll(function(){
-
-var isElementInView = isScrolledIntoView($('.renders'));
-
-  if (isElementInView) {
-    console.log(isElementInView);
-    // stopScroll('.renders .slick-track')
-
-} else {
-    console.log('out of view');
-    // return;
-}
-});
-
-// $(function() {
-
-//    $("body").mousewheel(function(event, delta) {
-
-//       this.scrollLeft -= (delta * 30);
-    
-//       event.preventDefault();
-
-//    });
-
-// });
-
-// var delta;
-// var currentSlideIndex = 0;
-// var scrollTimes;
-
-// function elementScroll (e) {
-
-//   // --- Scrolling up ---
-//   if (e.originalEvent.detail < 0 || e.originalEvent.wheelDelta > 0) { 
-
-//     delta--;
-
-//     if ( Math.abs(delta) >= scrollThreshold) {
-//     console.log('scroll-down');
-//     scrollTimes += 1;
-//     }
-//   }
-
-//   // --- Scrolling down ---
-//   else {
-
-//     delta++;
-
-//     if (delta >= scrollThreshold) {
-//       console.log('scroll-up');
-//       scrollTimes += 1;
-//     }
-//   }
-
-//   console.log("Scroll Times = " + scrollTimes);
-//   // Prevent page from scrolling
-//   return false;
-// }
-
-
-// $(window).on({
-//   'DOMMouseScroll mousewheel': elementScroll
-// });
